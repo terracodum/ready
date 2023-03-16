d402153d4410e5e072b4acb0cbbc09073d5d08df
huinyacode <127498985+huinyacode@users.noreply.github.com>
2023-03-16 18:08:18 +0600

    tt

--- dz.py
+++ dz.py
@@ -1,20 +1,9 @@
-letters = "01234567"
-even = "0246"
-letters = sorted(letters)
-result = ""
-counter = 0
-exitflag = False
-for a in letters:
-    for b in letters:
-        for c in letters:
-            for d in letters:
-                for e in letters:
-                    for f in letters:
-                        result = a + b + c + d + e + f
-                        if (result[0]<result[2] and result[0]<result[3] \
-                            and result[0]<result[4] and result[0]<result[5]) \
-                            and (result[1]<result[2] and result[1]<result[3] \
-                            and result[1]<result[4] and result[1]<result[5]):
-                                for i in range(0,4):
-                                    counter +=1
-                                    print(counter)
+long = []
+for i in range(101,1000):
+    string ="1"*i
+    while "111" in string:
+        string = string.replace("111", "22", 1)
+        string = string.replace("222", "11", 1)
+    if string.count("1")==1:
+        long.append(i)
+print(min(long))
