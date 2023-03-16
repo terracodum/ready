3f3278dced866d3625a58e96bb408e60b127d1e4
huinyacode <127498985+huinyacode@users.noreply.github.com>
2023-03-16 18:05:41 +0600

    dodo

--- task even.py
+++ task even.py
@@ -1,25 +1,5 @@
-letters = "01234567"
-letters = sorted(letters)
-even = "024"
-odd = "1357"
-print(letters)
-result = ""
-counter = 0
-for a in letters:
-    for b in letters:
-        for c in letters:
-            for d in letters:
-                for e in letters:
-                    result = a + b + c + d + e
-
-                    if result.count("6") == 1 and result[0] == "6" and result[1] in even:
-
-                        counter += 1
-                    elif result.count("6") == 1 and result[-1] == "6" and result[-2] in even and result[0] != "0":
-                        counter += 1
-                    elif result.count("6") == 1 and result[0] != "0":
-                        indexres = result.index("6")
-                        if result[indexres - 1] in even and result[indexres + 1] in even:
-                            counter += 1
-                            break
-print(counter)
+def f(a):
+    for i in range(2, (a // 2) + 1):
+        if a % i == 0:
+            return False
+    return True
