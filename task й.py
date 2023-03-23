ac7ef4163985f44d04cace64671b79fb3287429c
huinyacode <127498985+huinyacode@users.noreply.github.com>
2023-03-23 18:54:41 +0600

    dodo

--- task й.py
+++ task й.py
@@ -1,18 +1,24 @@
-counter = 0
-even = [2, 4, 6, 8, 0]
-for i in range(1000,10000):
-    stri = str(i)
-    flag = True
-    for j in range(0, 4):
-        if int(stri[j]) in even:
-            flag = False
-    num1 = int(stri[0]) + int(stri[1])
-    num2 = int(stri[2]) + int(stri[3])
-    array = [num1, num2]
-    array = sorted(array)
-    array = map(str,array)
-    result = "".join(array)
-    if result =="414" and flag:
-        counter +=1
-        print(i)
-print(counter)
+def solution(s):
+    indexi = []
+    flag = False
+    for i in range(len(s)):
+        sl = s[i:]
+        finder = sl[0].isupper()
+        if finder:
+            indexi.append(i)
+            flag = True
+        else:
+            continue
+    if flag:
+        oldj = 0
+        res = ""
+        for j in indexi:
+            print(j, oldj)
+            sR = s[oldj:j:]
+            res += " " + "".join(sR)
+
+            oldj = j
+        res = res + " " + "".join(s[oldj::])
+        return res
+    else:
+        return s
