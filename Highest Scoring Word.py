5fef0d56af42afdbc285a463b36d46a1631eaf56
huinyacode <127498985+huinyacode@users.noreply.github.com>
2023-04-22 21:18:33 +0600

    dodo

--- Highest Scoring Word.py
+++ Highest Scoring Word.py
@@ -1,9 +1,13 @@
-import string
+from  string import ascii_lowercase as letters
 def high(x):
-    for i in range(len(string.ascii_lowercase)):
-
-
-
-
-
-print((high('what time are we climbing up the volcano')))
+    x = x.split(" ")
+    scores = {}
+    for word in range(len(x)):
+        current_score = 0
+        for let in range(len(letters)):
+            let_score = let + 1
+            current_score += x[word].count(letters[let])*let_score
+        scores.setdefault(x[word], current_score)
+    d = dict([max(scores.items(), key=lambda k_v: k_v[1])])
+    for i in d:
+        return i
